b1dd62771750edb078d422281752544e55ea9207
Rosiewww <86192909+Rosiewww@users.noreply.github.com>
2021-10-08 04:25:06 +0800

    单人博饼更新

--- Lab4_Bobing_App/Lab4_Bobing_App/ViewController.swift
+++ Lab4_Bobing_App/Lab4_Bobing_App/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var Zhuangyuanlbl: UILabel!
+    @IBOutlet weak var Resultshowlbl: UILabel!
+    @IBOutlet weak var ResultINGbtn: UIButton!
+    @IBOutlet weak var Rollnowbtn: UIButton!
+    @IBOutlet weak var Beforebtn: UIButton!
+    @IBOutlet weak var Diceimage6: UIImageView!
+    @IBOutlet weak var Diceimage5: UIImageView!
+    @IBOutlet weak var Diceimage4: UIImageView!
+    @IBOutlet weak var Diceimage3: UIImageView!
+    @IBOutlet weak var Diceimage2: UIImageView!
+    @IBOutlet weak var Diceimage1: UIImageView!
+    
     @IBOutlet weak var Nextbtn: UIButton!
     @IBOutlet weak var Beforeallpage: UIImageView!
     override func viewDidLoad() {
